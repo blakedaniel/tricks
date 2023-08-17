3fec5286cfca9afe5f74c1276b59f6efb6aa1c79
blakedaniel <blake.vanfleteren@gmail.com>
2023-08-17 10:02:21 -0400

    updated formatting

---
+++ tricks/tricks/settings_dev.py
@@ -0,0 +1,136 @@
+from pathlib import Path
+import dj_database_url
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = 'dfb009c0be19574fc76d3e8193ad22e67ee43a63bedd039f'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1',
+                 '0.0.0.0',
+                 'tricks.fly.dev',]
+
+INTERNAL_IPS = ['127.0.0.1',
+                '::1',]
+
+CSRF_TRUSTED_ORIGINS =['http://localhost*',
+                       'https://tricks.fly.dev',]
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # Third-party apps
+    "django_htmx",
+    "tailwind",
+    "theme",
+    "django_browser_reload",
+    # Local apps
+    "main.apps.MainConfig"
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Third-party middleware
+    "django_htmx.middleware.HtmxMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
+]
+
+ROOT_URLCONF = "tricks.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "tricks.wsgi.application"
+
+
+# Database
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://admin:asdf1234@0.0.0.0:5433/tricks',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
+
+
+# Password validation
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main','static'),
+                    os.path.join(BASE_DIR, 'theme','static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Default primary key field type
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# sessions configuration
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_COOKIE_AGE = 86400
+
+# login/out redirect
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+# tailwind
+TAILWIND_APP_NAME = 'theme'
